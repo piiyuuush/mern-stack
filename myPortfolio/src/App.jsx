1402537b8082eedc7a585d9d31cb7c71ef3a0c00
piiyuuush <piyush26102004bhandari@gmail.com>
2025-12-26 21:23:22 +0530

    naming conventions

--- myPortfolio/src/App.jsx
+++ myPortfolio/src/App.jsx
@@ -1,5 +1,5 @@
 import Navbar from "./components/Navbar";
-import Home from "./sections/Home";
+import Home from "./sections/home";
 import About from "./sections/About";
 import Edu from "./sections/Edu";
 import Skill from "./sections/skill";
